75def32daed32facbb4d53863f97d0c5df6c6d7e
Cristian Scholtz Chamy <cristian.scholtz@gmail.com>
2019-10-16 21:46:26 -0300

    Revert "merge_branch"
    This reverts commit fb382577

--- app/src/main/java/cscholtz/android/nutrievaluator/CacheUploadActivity.java
+++ app/src/main/java/cscholtz/android/nutrievaluator/CacheUploadActivity.java
@@ -3,7 +3,6 @@ import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,9 +11,7 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
-import org.apache.pdfbox.util.Charsets;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.File;
 import java.io.InputStream;
@@ -24,43 +21,35 @@ import java.util.Date;
 
 public class CacheUploadActivity extends AppCompatActivity {
 
+    private Button startButton;
     private TextView tiempo;
-    private String nombre;
-    private String sexo;
-    private String edad;
-    private Float[] medidas = new Float[2];/*peso,talla*/
-    private Integer[] pliegues = new Integer[4];/*tricipital,bicipital,suprailiaco,subescapular*/
-    private Float[] circunferencias = new Float[4]; /*cintura,cadera,braquial,carpo*/
-    private sqliteOpenHelper helper = new sqliteOpenHelper(this,"BD1",null,1); //clase que crea y hace consultas a la base de datos
-    //reciben los string de texto a poner el el pdf, con valores obtenidos del evaluador
-    private String imc;
-    private String ipt;
-    private String pesoideal;
-    private String cmb;
-    private String amb;
-    private String agb;
-    private String pt;
-    private String cin;
-    private String relcincad;
-    private String contextura;
+
+    private String nombre, sexo,edad,peso,talla,cintura,cadera,braquial,carpo,tricipital,bicipital,suprailiaco,subescapular;//reciben los parametros de los inputs
+    private SQLiteOpen_Helper helper = new SQLiteOpen_Helper(this,"BD1",null,1); //clase que crea y hace consultas a la base de datos
+    private String IMC,IPT,PESO_IDEAL,CMB,AMB,AGB,PT,CIN,RELCINCAD,CONTEXTURA; //reciben los string de texto a poner el el pdf, con valores obtenidos del evaluador
+
+    //Clase para crear el archivo PDF
+    private TemplatePDF templatePDF;
     //Guarda el nombre del archivo PDF actual
-    private String fileName;
+    private String FileName;
+
     //Referencia a Storage para acceder a FirebaseStorage en la Cloud
     private StorageReference storageReference;
+
     //JasonObjhect para recorrer el archivo .json
     private JSONObject jsonObject;
+    private int len; // cantidad de input a sacar del archivo .json
+
     //Variables para hacer la medicion dl tiempo de ejecucion de la tarea
     private SimpleDateFormat tsf;
-    private String t1;
-    private String t2;
-    private Date d1;
-    private Date d2;
+    private String t1, t2; //Time inicial y final
+    private Date d1, d2; //Para obtener tiempo inicial y final
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cache_upload);
-        Button startButton = (Button) findViewById(R.id.startButtonCache);
+        startButton = (Button) findViewById(R.id.startButtonCache);
         tiempo = (TextView) findViewById(R.id.tiempoCache);
 
         tsf = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss.SSS");
@@ -72,12 +61,12 @@ public class CacheUploadActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 t1 = tsf.format(new Date());
-                executeFunctions();
+                ExecuteFunctions();
             }
         });
     }
     
-    public void executeFunctions() {
+    public void ExecuteFunctions() {
         String jsonString;
         try {
             InputStream is = getAssets().open("inputs_example.json");
@@ -85,30 +74,31 @@ public class CacheUploadActivity extends AppCompatActivity {
             byte[] buffer = new byte[size];
             is.read(buffer);
             is.close();
-            jsonString = new String(buffer, Charsets.UTF_8);
+            jsonString = new String(buffer,"UTF-8");
             JSONArray jsonArray = new JSONArray(jsonString);
             PDFMergerUtility ut = new PDFMergerUtility();
-            int len = 50;
+            len = 20;
             for(int i = 0;i<len; i++){
                 jsonObject = jsonArray.getJSONObject(i);
-                inputSetter();
-                evaluateData();
-                createPDF();
-                ut.addSource(Environment.getExternalStorageDirectory().toString()+"/PDF/"+ fileName +".pdf");
+                InputSetter();
+                EvaluateData();
+                CreatePDF();
+                ut.addSource(Environment.getExternalStorageDirectory().toString()+"/PDF/"+FileName+".pdf");
             }
             ut.setDestinationFileName(Environment.getExternalStorageDirectory().toString()+"/PDF/MergedPDF.pdf");
             ut.mergeDocuments();
-            uploadFile();
+            UploadFile();
+
         }catch (Exception e){
-            Log.e("ExecuteFunctions", e.toString());
+            e.printStackTrace();
         }
     }
 
-    public void uploadFile(){
+    public void UploadFile(){
         File f1 = new File(Environment.getExternalStorageDirectory().toString()+"/PDF/MergedPDF.pdf");
-        Uri uriFile = Uri.fromFile(f1);
+        Uri uri_file = Uri.fromFile(f1);
         StorageReference stg = storageReference.child("Cache").child(f1.getName());
-        stg.putFile(uriFile)
+        stg.putFile(uri_file)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -117,79 +107,71 @@ public class CacheUploadActivity extends AppCompatActivity {
                             d1 = tsf.parse(t1);
                             d2 = tsf.parse(t2);
                         } catch (ParseException e) {
-                            Log.e("SuccessListener", e.toString());
+                            e.printStackTrace();
                         }
                         long diff = d2.getTime()-d1.getTime();
                         tiempo.setText(String.valueOf(diff)+" miliseconds");
                     }
                 });
+
     }
 
-    public void createPDF(){
+    public void CreatePDF(){
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         String currentDate = sdf.format(new Date());
-        TemplatePDF templatePDF;
         templatePDF = new TemplatePDF(getApplicationContext());
-        fileName = nombre+"_"+currentDate;
-        templatePDF.openDocument(fileName);
+        FileName = nombre+"_"+currentDate;
+        templatePDF.openDocument(FileName);
         templatePDF.addMetaData("Evaluacion Nutricional"+nombre,"evaluacion","cs");
         templatePDF.addTitles("Evaluacion Nutricional","Paciente: "+nombre,currentDate);
-        templatePDF.addParagraph(imc);
-        templatePDF.addParagraph(ipt);
-        templatePDF.addParagraph(pesoideal);
-        templatePDF.addParagraph(cmb);
-        templatePDF.addParagraph(amb);
-        templatePDF.addParagraph(agb);
-        templatePDF.addParagraph(pt);
-        templatePDF.addParagraph(cin);
-        templatePDF.addParagraph(relcincad);
-        templatePDF.addParagraph(contextura);
+        templatePDF.addParagraph(IMC);
+        templatePDF.addParagraph(IPT);
+        templatePDF.addParagraph(PESO_IDEAL);
+        templatePDF.addParagraph(CMB);
+        templatePDF.addParagraph(AMB);
+        templatePDF.addParagraph(AGB);
+        templatePDF.addParagraph(PT);
+        templatePDF.addParagraph(CIN);
+        templatePDF.addParagraph(RELCINCAD);
+        templatePDF.addParagraph(CONTEXTURA);
         templatePDF.closeDocument();
     }
 
-    public void evaluateData(){
-        Evaluator ev = new Evaluator(nombre, sexo, new Integer(edad),  medidas, pliegues, circunferencias);
+    public void EvaluateData(){
+        Evaluator E = new Evaluator(nombre, sexo, new Integer(edad),  new Integer(tricipital),  new Integer(bicipital),  new Integer(suprailiaco), new Integer(subescapular),  new Float(peso),  new Float(talla),  new Float(cintura),  new Float(cadera),  new Float(braquial),  new Float(carpo));
         helper.abrir();
         String e = helper.getIdEdad(new Integer(edad));
-        imc = "IMC: "+String.format("%.2f",ev.getIMC()) + " kg/mt² "+ev.evaluarIMC();
-        ipt = "%IPT: "+String.format("%.2f",ev.getIPT())+"% "+ev.evaluarIPT();
-        pesoideal = "PESO IDEAL: "+String.format("%.2f",ev.getPesoIdeal())+" kg";
-        Integer[] rCMB = ev.rangoPercentiles(ev.getCMB(),ev.percentiles(helper.percentiles(e,sexo,"CMB")));
-        cmb = "CMB: "+mmFloat(ev.getCMB())+percentilRange(rCMB[0],rCMB[1])+ev.evaluarPercentilesCMB(rCMB[0],rCMB[1]);
-        Integer[] rAMB = ev.rangoPercentiles(ev.getAMB(),ev.percentiles(helper.percentiles(e,sexo,"AMB")));
-        amb = "AMB: "+mmFloat(ev.getAMB())+percentilRange(rAMB[0],rAMB[1])+ev.evaluarPercentiles(rAMB[0],rAMB[1]);
-        Integer[] rAGB = ev.rangoPercentiles(ev.getAGB(),ev.percentiles(helper.percentiles(e,sexo,"AGB")));
-        agb = "AGB: "+mmFloat(ev.getAGB())+percentilRange(rAGB[0],rAGB[1])+ev.evaluarPercentiles(rAGB[0],rAGB[1]);
-        Integer[] rPT = ev.rangoPercentiles(ev.getPT(),ev.percentiles(helper.percentiles(e,sexo,"PT")));
-        pt = "PT: "+mmFloat(ev.getPT())+percentilRange(rPT[0],rPT[1])+ev.evaluarPercentiles(rPT[0],rPT[1]);
-        cin = "CINTURA: "+String.format("%.2f",ev.getCin())+" cm "+ev.evaluarCintura();
-        relcincad = "REL CINT/CAD: "+String.format("%.2f",ev.getRelCinCad())+" "+ev.evaluarRelCinCad();
-        contextura = "CONTEXTURA: "+String.format("%.2f",ev.getContextura())+" "+ev.evaluarContextura();
+        IMC = "IMC: "+String.format("%.2f",E.getIMC()) + " kg/mt² "+E.evaluarIMC();
+        IPT = "%IPT: "+String.format("%.2f",E.getIPT())+"% "+E.evaluarIPT();
+        PESO_IDEAL = "PESO IDEAL: "+String.format("%.2f",E.getPesoIdeal())+" kg";
+        Integer[] rCMB = E.rangoPercentiles(E.getCMB(),E.Percentiles(helper.percentiles(e,sexo,"CMB")));
+        CMB = "CMB: "+String.format("%.0f",E.getCMB())+ " mm (P"+rCMB[0]+"- P"+rCMB[1]+") "+E.evaluarPercentilesCMB(rCMB[0],rCMB[1]);
+        Integer[] rAMB = E.rangoPercentiles(E.getAMB(),E.Percentiles(helper.percentiles(e,sexo,"AMB")));
+        AMB = "AMB: "+String.format("%.0f",E.getAMB())+ " mm (P"+rAMB[0]+"- P"+rAMB[1]+") "+E.evaluarPercentiles(rAMB[0],rAMB[1]);
+        Integer[] rAGB = E.rangoPercentiles(E.getAGB(),E.Percentiles(helper.percentiles(e,sexo,"AGB")));
+        AGB = "AGB: "+String.format("%.0f",E.getAGB())+ " mm (P"+rAGB[0]+"- P"+rAGB[1]+") "+E.evaluarPercentiles(rAGB[0],rAGB[1]);
+        Integer[] rPT = E.rangoPercentiles(E.getPT(),E.Percentiles(helper.percentiles(e,sexo,"PT")));
+        PT = "PT: "+String.format("%.0f",E.getPT())+" mm (P"+rPT[0]+"- P"+rPT[1]+") "+E.evaluarPercentiles(rPT[0],rPT[1]);
+        CIN = "CINTURA: "+String.format("%.2f",E.getCin())+" cm "+E.evaluarCintura();
+        RELCINCAD = "REL CINT/CAD: "+String.format("%.2f",E.getRelCinCad())+" "+E.evaluarRelCinCad();
+        CONTEXTURA = "CONTEXTURA: "+String.format("%.2f",E.getContextura())+" "+E.evaluarContextura();
         helper.cerrar();
     }
 
-    public String mmFloat(double f){
-        return String.format("%.0f",f)+"mm ";
-    }
-
-    public String percentilRange(Integer p1, Integer p2){
-        return "(P"+p1+"- P"+p2+") ";
-    }
-
-    public void inputSetter()throws JSONException {
+    public void InputSetter()throws Exception{
         nombre = jsonObject.getString("nombre");
         sexo = jsonObject.getString("sexo");
         edad = jsonObject.getString("edad");
-        medidas[0] = Float.parseFloat(jsonObject.getString("peso"));
-        medidas[1] = Float.parseFloat(jsonObject.getString("talla"));
-        circunferencias[0] = Float.parseFloat(jsonObject.getString("cintura"));
-        circunferencias[1] = Float.parseFloat(jsonObject.getString("cadera"));
-        circunferencias[2] = Float.parseFloat(jsonObject.getString("braquial"));
-        circunferencias[3] = Float.parseFloat(jsonObject.getString("carpo"));
-        pliegues[0] = Integer.valueOf(jsonObject.getString("tricipital"));
-        pliegues[1] = Integer.valueOf(jsonObject.getString("bicipital"));
-        pliegues[2] = Integer.valueOf(jsonObject.getString("suprailiaco"));
-        pliegues[3] = Integer.valueOf(jsonObject.getString("subescapular"));
+        peso = jsonObject.getString("peso");
+        talla = jsonObject.getString("talla");
+        cintura = jsonObject.getString("cintura");
+        cadera = jsonObject.getString("cadera");
+        braquial = jsonObject.getString("braquial");
+        carpo = jsonObject.getString("carpo");
+        tricipital = jsonObject.getString("tricipital");
+        bicipital = jsonObject.getString("bicipital");;
+        suprailiaco = jsonObject.getString("suprailiaco");
+        subescapular = jsonObject.getString("subescapular");
     }
 
 }
